91591f616ea5e93aff503446877de20b758b5126
Иван <ivan07m@gmail.com>
2021-05-30 01:26:57 +0300

    "parser"

---
+++ bot.py
@@ -0,0 +1,15 @@
+from parser import Avito
+
+import telebot
+
+bot = telebot.TeleBot('1894033956:AAGkD_yFVqY4E1s2-Qinw3hnTtuCsTzDSxs')
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_parse_result(message):
+    parse = Avito()
+    result = parse.city('').min_price('').max_price('').search_object('камри').get()
+    [bot.reply_to(message, ''.join(str(res))) for res in result]
+
+
+bot.polling(none_stop=True, interval=2)
