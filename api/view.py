66ac0c7f07e429b51a0137f5110183e59ee77553
Иван <ivan07m@gmail.com>
2021-11-06 01:44:55 +0300

    fix

--- api/view.py
+++ api/view.py
@@ -6,16 +6,19 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 from config.config import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD
+from utils.decorator import info_logger
 
 redis = Redis(REDIS_HOST, REDIS_PORT, REDIS_PASSWORD)._connect()
 
 
+@info_logger
 @api_view(['GET'])
 @permission_classes([permissions.IsAdminUser])
 def redis_view(request):
     return Response(json.loads(json.dumps(redis.get_all())), status=status.HTTP_200_OK)
 
 
+@info_logger
 @api_view(['GET'])
 @permission_classes([permissions.IsAdminUser])
 def redis_detail_view(request, user_id):
--- utils/decorator.py
+++ utils/decorator.py
@@ -1,9 +1,15 @@
+from django.core.handlers.wsgi import WSGIRequest
+
 from logger import log
 
 
 def info_logger(func):
     def wrapper(*args, **kwargs):
-        log.info(args[0])
+        if isinstance(args[0], WSGIRequest):
+            log.info(str(list(str(args[0].META).split(','))[70::]))
+            return func(*args, **kwargs)
+        else:
+            log.info(str(args[0]) + '\n')
         func(*args, **kwargs)
     return wrapper
 
