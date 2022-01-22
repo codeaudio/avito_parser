f1f29fb93ca3d457a0dca849846a23a86886beab
Иван <ivan07m@gmail.com>
2022-01-22 20:59:35 +0300

    fix adapt

--- config/htmlclass.py
+++ config/htmlclass.py
@@ -1,7 +1,5 @@
-SPAN_CLASS = 'price-text-E1Y7h text-text-LurtD text-size-s-BxGpL'
-DIV_CLASS = 'iva-item-text-_s_vh iva-item-description-S2pXQ text-text-LurtD text-size-s-BxGpL'
+SPAN_CLASS = 'price-text-_YGDY text-text-LurtD text-size-s-BxGpL'
+DIV_CLASS = 'iva-item-text-Ge6dR iva-item-description-FDgK4 text-text-LurtD text-size-s-BxGpL'
 A_CLASS = (
-    'link-link-MbQDP link-design-default-_nSbv'
-    ' title-root-j7cja iva-item-title-_qCwt'
-    ' title-listRedesign-XHq38 title-root_maxHeight-SXHes'
+    'link-link-MbQDP link-design-default-_nSbv title-root-zZCwT iva-item-title-py3i_ title-listRedesign-_rejR title-root_maxHeight-X6PsH'
 )
